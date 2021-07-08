93a2ffd69508206a03fcaad0b7d3d7263f4da1bc
Mob <wj0199@mail.ustc.edu.cn>
2021-07-07 18:47:31 -0700

    modify the test and the stream buffer

--- rust_freertos/examples/test_stream_buffer.rs
+++ rust_freertos/examples/test_stream_buffer.rs
@@ -4,30 +4,34 @@ extern crate rust_freertos;
 
 use rust_freertos::*;
 use simplelog::*;
-use rust_freertos::stream_buffer::*;
-use std::sync::Arc;
+
 
 fn main() { // test streambuffer
-    // 两个任务共享所有权，所以需Arc包装。
-    let buffer_recv = Arc::new(StreamBuffer::new(10));
-    let buffer_sender = Arc::clone(&buffer_recv);
+   
+
+    let mut sender_buffer = stream_buffer::StreamBufferHandle::
+         StreamBufferGenericCreate(5,1,true ,5);
+
+    let mut receiver_buffer = sender_buffer.clone();
     let _ = TermLogger::init(LevelFilter::Trace, Config::default());
     // 发送数据的任务代码。
     let sender = move || {
         for i in 1..11 {
             // send方法的参数包括要发送的数据、最大发送值和 ticks_to_wait
-            buffer_sender.send(i, 1024, pdMS_TO_TICKS!(50)).unwrap();
+            sender_buffer.StreamBufferSend(i, 5, pdMS_TO_TICKS!(50));
         }
         loop {
         }
     };
     // 接收数据的任务代码。
     let receiver = move || {
+        let mut x :u8 = 0;
         let mut sum = 0;
         loop {
             // receive方法的参数只有ticks_to_wait和 最大接受值
-            if let Ok(x) = buffer_recv.receive(1, pdMS_TO_TICKS!(10)) {
-                println!("{}", x);
+            let num = receiver_buffer.StreamBufferReceive(&mut x, 1,pdMS_TO_TICKS!(10));
+            if num > 0{
+                            println!("{}", num);
                 sum += x;
             } else {
                 trace!("receive END");
@@ -37,14 +41,18 @@ fn main() { // test streambuffer
             }
         }
     };
-    // 创建这两个任务。
+    //创建这两个任务。
     let _sender_task = task_control::TCB::new()
         .name("Sender")
-        .priority(3)
+        .priority(4)
         .initialise(sender);
     let _receiver_task = task_control::TCB::new()
         .name("Receiver")
         .priority(3)
-        .initialise(receiver);     
+        .initialise(receiver);
+        
     kernel::task_start_scheduler();
+
+
+
 }
